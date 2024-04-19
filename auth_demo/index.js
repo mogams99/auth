fd16e3800ba4e7721314f36cdf6c055690a59ffb
Mochammad Gamal <mogams1811@gmail.com>
2024-04-19 09:57:29 +0700

    chore: :construction: register new user with bcrypt

--- auth_demo/index.js
+++ auth_demo/index.js
@@ -1,23 +1,50 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const app = express();
+
+// * Default app port and url
 const port = 3012;
 const url = `http//localhost:${port}`;
 
-// ? import model
+// ? Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1/auth-demo').then(res => {
+    console.log('MongoDB is connected');
+}).catch(err => {
+    console.log(err);
+})
+
+// ? Import model
 const User = require('./models/User');
 
-// ! config app
+// ! Config app
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.use(express.urlencoded({ extended: true }));
 
-// ? our routes
+// ? Our routes
+app.get('/', (req, res) => {
+    res.send('This home page.');
+});
 app.get('/register', (req, res) => {
     res.render('register');
 });
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    const user = new User({
+        username,
+        password: hashedPassword
+    });
+    await user.save();
+    res.redirect('/');
+});
+
 app.get('/admin', (req, res) => {
     res.send('Admin page has login required.')
 });
 
+// * Check listen app
 app.listen(3012, () => {
     console.log(`App listen on port: ${url}`);
 });
